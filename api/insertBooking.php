26b05c6de2ab3bd038ae8f4cfeb2c92a0f43dba1
Helena Edblad <helena.edblad@gmail.com>
2018-09-03 20:44:43 +0200

    small fixes

--- api/insertBooking.php
+++ api/insertBooking.php
@@ -1,7 +1,6 @@
 <?php
 require 'database.php';
 
-
 $request_body = file_get_contents('php://input');
 $bookingData = json_decode($request_body);
 
@@ -10,14 +9,15 @@ var_dump($bookingData);
 $customerQuery = 'INSERT INTO customer (name, phone, email) VALUES (:name, :phone, :email)';
 $bookingQuery = 'INSERT INTO bookings (customer_id, date, time, amount_of_people)
                      VALUES (LAST_INSERT_ID(), :date, :time, :amount_of_people)';
- $statement = $pdo->prepare($customerQuery);
-$statement->execute(array(
+$customerStatement = $pdo->prepare($customerQuery);
+$customerStatement->execute(array(
     ":name" => $bookingData->name,
     ":phone" => $bookingData->phone,
     ":email" => $bookingData->email
 ));
- $statement2 = $pdo->prepare($bookingQuery);
-$statement2->execute(array(
+ 
+$bookingStatement = $pdo->prepare($bookingQuery);
+$bookingStatement->execute(array(
     ":date" => $bookingData->date,
     ":time" => $bookingData->time,
     ":amount_of_people" => $bookingData->numberOfGuests
@@ -25,9 +25,9 @@ $statement2->execute(array(
  //echo json_encode($booking);
 
  //Send booking confirmation email to customer upon completion of booking
- $headers = "From: bookingconfirmation@restaurant.com";
- mail($bookingData->email,"Booking Confirmation","<h1>Thank you for your booking $bookingData->name</h1><br> Your have booked a table for
- $bookingData->numberOfGuests, on the date of $bookingData->date, at $bookingData->time . We look forward to seeing you!" $headers);
+//  $headers = "From: bookingconfirmation@restaurant.com";
+//  mail($bookingData->email,"Booking Confirmation","<h1>Thank you for your booking $bookingData->name</h1><br> Your have booked a table for
+//  $bookingData->numberOfGuests, on the date of $bookingData->date, at $bookingData->time . We look forward to seeing you!" $headers);
 
 
 echo '';
