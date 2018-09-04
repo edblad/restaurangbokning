6f9d7ffd95f3ccf84ef78e449d9e8056f62cb40c
andxz <andrez.romero@hotmail.com>
2018-09-04 14:39:47 +0200

    hamburger menu not working

--- restaurangbokning/src/components/menu/menu.js
+++ restaurangbokning/src/components/menu/menu.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './menu.css';
 import { Link } from 'react-router-dom';
 import iconHome from '../.././images/iconHome.svg';
 
-const Menu = () => (
+
+class Menu extends Component {
+    
+        state = {
+            isHamburgerMenuHidden: true
+        }
+
+
+    toogleHamburgerMenu = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            isHamburgerMenuHidden: false
+        });
+        
+            }
+
+  render() {
+      
+    const addHamburgerStyle = this.state.isHamburgerMenuHidden ? { display: 'none'} : {};
+
+    return (
+        
   <div className="menu">
+    <div className="normalMenu">
     <Link to="/">Home</Link>
     <Link to="/booking">Booking</Link>
     {/* <img src={iconHome} alt="Icon for home" className="iconHome"/> */}
     <div className="circle"><span>SIX</span></div>
     <Link to="/contact">Contact</Link>
     <Link to="/admin">Admin</Link>
-  </div>
-);
+    </div>
+    
+    <div className="hamburgerMenu" onClick={this.toogleHamburgerMenu}>
+    <nav class="menu-opener" >
+        <div class="menu-opener-inner"></div>
+    </nav>
+    <nav class="menuB" style={addHamburgerStyle}>
+    <ul class="menu-inner">
+    <Link to="/">Home</Link>
+    <Link to="/booking">Booking</Link>
+    <Link to="/contact">Contact</Link>
+    <Link to="/admin">Admin</Link>
+    </ul>
+  </nav>
+    </div></div>
+        
+    );
+  }
+}
 
 export default Menu;
