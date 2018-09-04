3dbef9011ae7f61a7f2b0391988a9a889ccedc8e
andxz <andrez.romero@hotmail.com>
2018-09-04 12:58:26 +0200

    info for homepage

--- restaurangbokning/src/pages/home/home.js
+++ restaurangbokning/src/pages/home/home.js
@@ -6,8 +6,9 @@ import { Link } from 'react-router-dom';
 const Home = () => (
   <div className="page-home">
     <div className="homeBox">
-      <h1>Lorem ipsum</h1>
-      <p>Lorem ipsum dolor sit amet, cum fierent assentior te. Nec meliore gubergren ex. Eum movet vidisse placerat ad, in vix sumo ponderum perfecto</p>
+      <h1>Six</h1>
+      <p>Six is a central restaurant serving a menu inspired by the vibrant city of Stockholm. Award-winning
+ Chef Michael Cimarusti helms the menu and Chef Crisi Echiverri oversees desserts.</p>
       <Link to="/Contact">
         <Button className="button ghost homeButton" text="Find us" />
       </Link>
