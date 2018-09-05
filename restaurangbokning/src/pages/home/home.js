4db70af82e9a2657f29edc7738d03b28bd65ac08
Helena Edblad <helena.edblad@gmail.com>
2018-09-05 14:16:15 +0200

    add some text on startpage

--- restaurangbokning/src/pages/home/home.js
+++ restaurangbokning/src/pages/home/home.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Home = () => (
   <div className="page-home">
     <div className="homeBox">
-      <h1>Six</h1>
+      <h1>Welcome to Six</h1>
       <p>Six is a central restaurant serving a menu inspired by the vibrant city of Stockholm. Award-winning
  Chef Michael Cimarusti helms the menu and Chef Crisi Echiverri oversees desserts.</p>
       <Link to="/Contact">
